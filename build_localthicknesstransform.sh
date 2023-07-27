7384ee11af972d9a08398ff3f791266348398dd8
brunsst <91964829+brunsst@users.noreply.github.com>
2023-07-27 12:16:06 +0200

    Update build_localthicknesstransform.sh

--- build_localthicknesstransform.sh
+++ build_localthicknesstransform.sh
@@ -12,9 +12,9 @@ echo "compiling distancemapping.cpp"
 g++ -w -fexceptions -O3 -std=c++11 -fopenmp  -c $PWD"/Geometry/distancemapping.cpp" -o $PWD"/Build/distancemapping.o"
 echo "compiling localthicknesstransform.cpp"
 g++ -w -fexceptions -O3 -std=c++11 -fopenmp  -c $PWD"/Geometry/localthicknesstransform.cpp" -o $PWD"/Build/localthicknesstransform.o"
-echo "compiling main_cpu.cpp"
+echo "compiling main.cpp"
 g++ -w -fexceptions -O3 -std=c++11 -fopenmp  -c $PWD"/main.cpp" -o $PWD"/Build/main.o"
-echo "linking LocalTHicknessTransform"
+echo "linking LocalThicknessTransform"
 g++  -o $PWD/LocalThicknessTransform $PWD/Build/main.o  $PWD/Build/auxiliary.o $PWD/Build/hdcommunication.o $PWD/Build/distancemapping.o  $PWD/Build/localthicknesstransform.o  -ltiff -lgomp
 echo "--------------------------------------------------"
 
